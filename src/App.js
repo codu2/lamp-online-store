274761d92e291ab3325e5a47808393618a28c3ad
codu2 <82283517+codu2@users.noreply.github.com>
2021-12-21 11:25:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import './App.css';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Products from './components/products/Products';
-import User from './components/user/User';
+import { fetchCartData, sendCartData } from './store/cart-action';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart);
+  
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch])
+
+  useEffect(() => {
+    if(isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if(cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch])
+  
   return (
     <div className='container'>
       <Header />
       <Main />
       <Products />
-      <User />
     </div>
   )
 };
