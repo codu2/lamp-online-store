f4b3ece0148df131d00c0acf47be1a6051a4a812
codu2 <82283517+codu2@users.noreply.github.com>
2021-12-22 09:47:48 +0900

    Update ProductItem.js

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import classes from './ProductItem.module.css';
+import { cartActions } from '../../store/cart-slice';
 
 const ProductItem = props => {
+   const dispatch = useDispatch();
+
+    const { id, name, price, color, img } = props;
+
+    const addItemToCartHandler = () => {
+        dispatch(cartActions.addItemToCart({
+            id,
+            name,
+            price,
+            color,
+            img
+        }))
+    };
+   
    return (
     <div className={classes['product-item']}>
-        <img src={props.img} alt="lamp" />
+        <img src={img} alt="lamp" />
         <div className={classes['product-box']}>
-            <div className={classes.name}>{props.name}</div>
+            <div className={classes.name}>{name}</div>
             <div className={classes['product-info']}>
-                <span>{props.color}</span>
-                <span>{`$${props.price}`}</span>
+                <span>{color}</span>
+                <span>{`$${price.toFixed(2)}`}</span>
             </div>
-            <button className={classes['add-button']}>
+            <button className={classes['add-button']} onClick={addItemToCartHandler}>
                 Add to Cart
             </button>
         </div>
