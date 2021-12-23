7c089643b4cbc64d286c3d572e50c45b6bb15ccd
codu2 <82283517+codu2@users.noreply.github.com>
2021-12-23 11:42:45 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import cartSlice from "./cart-slice";
+import uiSlice from "./ui-slice";
+import orderSlice from "./order-slice";
+import inputSlice from "./input-slice";
+import loginSlice from "./login-slice";
 
 const store = configureStore({
     reducer: {
-        cart: cartSlice.reducer
+        cart: cartSlice.reducer,
+        ui: uiSlice.reducer,
+        order: orderSlice.reducer,
+        input: inputSlice.reducer,
+        login: loginSlice.reducer
     }
 });
 
