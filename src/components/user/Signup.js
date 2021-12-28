8fb9ded4332ff27049635bc3f316cebb54effa3b
codu2 <82283517+codu2@users.noreply.github.com>
2021-12-28 12:02:28 +0900

    Update Signup.js

--- src/components/user/Signup.js
+++ src/components/user/Signup.js
@@ -1,32 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import classes from './Signup.module.css';
+import { signupActions } from '../../store/signup-slice';
+import { signupRequest } from '../../store/cart-action';
 
 const Signup = props => {
+    const dispatch = useDispatch();
+    const signup = useSelector(state => state.signup);
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+
+    const nameChangeHandler = event => {
+        setEnteredName(event.target.value);
+    }
+
+    const emailChangeHandler = event => {
+        setEnteredEmail(event.target.value);
+    }
+
+    const passwordChangeHandler = event => {
+        setEnteredPassword(event.target.value);
+    }
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        dispatch(signupActions.saveSignup({
+            name: enteredName,
+            email: enteredEmail,
+            password: enteredPassword
+        }));
+    };
+
+    useEffect(() => {
+        if(signup.name && signup.email && signup.password) {
+            dispatch(signupRequest(signup))
+        };
+
+        setEnteredName('');
+        setEnteredEmail('');
+        setEnteredPassword('');
+    }, [signup, dispatch])
+
     return (
-        <div className={classes['signup-form']}>
+        <form className={classes['signup-form']} onSubmit={submitHandler}>
             <h1>Signup</h1>
             <div className={classes['input-box']}>
                 <label htmlFor='name'>Name</label>
-                <input type='text' id="name" autoComplete='off' />
+                <input type='text' id="name" autoComplete='off' value={enteredName} onChange={nameChangeHandler} />
             </div>
             <div className={classes['input-box']}>
                 <label htmlFor='email'>E-mail</label>
-                <input type='text' id="email" autoComplete='off' />
+                <input type='text' id="email" autoComplete='off' value={enteredEmail} onChange={emailChangeHandler} />
             </div>
             <div className={classes['input-box']}>
                 <label htmlFor='password'>Password</label>
-                <input type='text' id="password" autoComplete='off' />
+                <input type='text' id="password" autoComplete='off' value={enteredPassword} onChange={passwordChangeHandler} />
             </div>
             <div className={classes['form-trans']}>
                 <p>Already have an account?</p>
-                <button onClick={props.onTrans}>Log in</button>
+                <button type="button" onClick={props.onTrans}>Log in</button>
             </div>
             <div className={classes.actions}>
                 <button className={classes['signup-button']}>Sign up</button>
-                <button className={classes['find-button']}>Help?</button>
+                <button type="button" className={classes['find-button']}>Help?</button>
             </div>
-        </div>
+        </form>
     )
 };
 
