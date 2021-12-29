9178cf742efc1bb1a2f71fa4b102d8dce9aaf6bd
codu2 <82283517+codu2@users.noreply.github.com>
2021-12-29 11:24:32 +0900

    Update signup-slice.js

--- src/store/signup-slice.js
+++ src/store/signup-slice.js
@@ -3,15 +3,67 @@ import { createSlice } from '@reduxjs/toolkit';
 const signupSlice = createSlice({
     name: 'signup',
     initialState: {
-        name: null,
-        email: null,
-        password: null
+       input: {
+            name: null,
+            email: null,
+            password: null
+        },
+        valid: {
+            nameIsValid: true,
+            emailIsValid: true,
+            passwordIsValid: true,
+            formIsValid: false
+        },
+        touched: {
+            name: false,
+            email: false,
+            password: false
+        }
     },
     reducers: {
         saveSignup(state, action) {
             state.name = action.payload.name;
             state.email = action.payload.email;
             state.password = action.payload.password;
+        },
+        touchedName(state, action) {
+            state.touched.name = action.payload;
+        },
+        touchedEmail(state, action) {
+            state.touched.email = action.payload;
+        },
+        touchedPassword(state, action) {
+            state.touched.password = action.payload;
+        },
+        nameFormIsValid(state, action) {
+            const isEmpty = value => value.trim() === '';
+
+            if(isEmpty(action.payload)) {
+                state.valid.nameIsValid = false;
+            } else {
+                state.valid.nameIsValid = true;
+            };
+        },
+        emailFormIsValid(state, action) {
+            const emailCheck = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+            if(!emailCheck.test(action.payload)) {
+                state.valid.emailIsValid = false;
+            } else {
+                state.valid.emailIsValid = true;
+            };
+        },
+        passwordFormIsValid(state, action) {
+            const passwordCheck = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*$?&]{8,16}$/;
+
+            if(!passwordCheck.test(action.payload)) {
+                state.valid.passwordIsValid = false;
+            } else {
+                state.valid.passwordIsValid = true;
+            };
+        },
+        signupFormIsValid(state) {
+            state.valid.formIsValid = state.valid.nameIsValid && state.valid.emailIsValid && state.valid.passwordIsValid;
         }
     }
 });
