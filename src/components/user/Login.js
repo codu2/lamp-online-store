fd7162ec5b58a7a0403421c536079ea1b2cb6fe2
codu2 <82283517+codu2@users.noreply.github.com>
2021-12-31 12:35:32 +0900

    Update Login.js

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -5,100 +5,36 @@ import classes from './Login.module.css';
 import { inputActions } from '../../store/input-slice';
 import { sendUserData } from '../../store/cart-action';
 import { loginActions } from '../../store/login-slice';
+import { uiActions } from '../../store/ui-slice';
 
 const Login = props => {
     const dispatch = useDispatch();
     const login = useSelector(state => state.input.login);
-    const beStoredLogin = useSelector(state => state.login);
+    const beStoredLogin = useSelector(state => state.login.input);
     const touched = useSelector(state => state.input.touched);
+    const users = useSelector(state => state.signup.users);
+    const exist = useSelector(state => state.login.exist);
 
-    const [enteredName, setEnteredName] = useState('');
     const [enteredEmail, setEnteredEmail] = useState('');
     const [enteredPassword, setEnteredPassword] = useState('');
     const [isSubmitted, setIsSubmitted] = useState(true);
     
-    const submitHandler = event => {
-        event.preventDefault();
-
-        if(!login.nameIsValid) {
-            dispatch(inputActions.touchedName(true));
-        };
-        
-        if(!login.emailIsValid) {
-            dispatch(inputActions.touchedEmail(true));
-        };
-        
-        if(!login.passwordIsValid) {
-            dispatch(inputActions.touchedPassword(true));
-        };
-
-        dispatch(inputActions.nameFormIsValid(enteredName));
-        dispatch(inputActions.emailFormIsValid(enteredEmail));
-        dispatch(inputActions.passwordFormIsValid(enteredPassword));
-
-        dispatch(loginActions.saveLogin({
-            name: enteredName,
-            email: enteredEmail,
-            password: enteredPassword
-        }));
-
-        dispatch(inputActions.loginInputFormIsValid());
-        
-        if(!login.formIsValid) {
-            setIsSubmitted(false);
-            return;
-        }
-    };
-
-    useEffect(() => {
-        if(login.formIsValid) {
-            dispatch(inputActions.touchedName(false));
-            dispatch(inputActions.touchedEmail(false));
-            dispatch(inputActions.touchedPassword(false));
-
-            dispatch(sendUserData({
-                name: beStoredLogin.name,
-                email: beStoredLogin.email,
-                password: beStoredLogin.password
-            }));
-            
-            setEnteredName('');
-            setEnteredEmail('');
-            setEnteredPassword('');
-        };
-    }, [beStoredLogin]);
-
     const isNotEmpty = event => event.target.value.trim() !== '';
     const emailCheck = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
     const passwordCheck = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*$?&]{8,16}$/;
-
-    const nameChangeHandler = event => {
-        dispatch(inputActions.touchedName(false));
-        if(touched.name) {
-            if(isNotEmpty) {
-                dispatch(inputActions.nameFormIsValid(event.target.value));
-            }
-        }
-        if(!isSubmitted) {
-            if(isNotEmpty) {
-                dispatch(inputActions.nameFormIsValid(event.target.value));
-            }
-        }
-        setEnteredName(event.target.value);
-    };
 
     const emailChangeHandler = event => {
         dispatch(inputActions.touchedEmail(false));  
         if(touched.email) {
             if(isNotEmpty) {
                 dispatch(inputActions.emailFormIsValid(event.target.value));
-            }
-        }
+            };
+        };
         if(!isSubmitted) {
             if(emailCheck.test(event.target.value.trim()) || isNotEmpty) {
                 dispatch(inputActions.emailFormIsValid(event.target.value));
-            }
-        }
+            };
+        };
         setEnteredEmail(event.target.value);
     };
 
@@ -107,23 +43,14 @@ const Login = props => {
         if(touched.password) {
             if(isNotEmpty) {
                 dispatch(inputActions.passwordFormIsValid(event.target.value));
-            }
-        }
+            };
+        };
         if(!isSubmitted) {
             if(passwordCheck.test(event.target.value.trim()) || isNotEmpty) {
                 dispatch(inputActions.passwordFormIsValid(event.target.value));
-            }
-        }
-        setEnteredPassword(event.target.value);
-    };
-    
-    const nameBlurHander = event => {
-        if(event.target.value.trim() === '') {
-            dispatch(inputActions.touchedName(true));
-        } else {
-            dispatch(inputActions.touchedName(false));
+            };
         };
-        dispatch(inputActions.nameFormIsValid(event.target.value));
+        setEnteredPassword(event.target.value);
     };
 
     const emailBlurHandler = event => {
@@ -144,7 +71,65 @@ const Login = props => {
         dispatch(inputActions.passwordFormIsValid(event.target.value));
     };
 
-    const nameClasses = `${classes['input-box']} ${login.nameIsValid && !touched.name ? '' : classes.invalid}`;
+    const submitHandler = event => {
+        event.preventDefault();
+
+        if(!login.emailIsValid) {
+            dispatch(inputActions.touchedEmail(true));
+        };
+        
+        if(!login.passwordIsValid) {
+            dispatch(inputActions.touchedPassword(true));
+        };
+        
+        for(const key in users) {
+            if(users[key].email === enteredEmail) {
+                dispatch(loginActions.existingEmail(true));
+                if(users[key].password === enteredPassword) {
+                    dispatch(loginActions.matchingPassword(true));
+                    break;
+                } else {
+                    dispatch(loginActions.matchingPassword(false));
+                };   
+            } else {
+                dispatch(loginActions.existingEmail(false));
+            };
+        };
+
+        dispatch(inputActions.emailFormIsValid(enteredEmail));
+        dispatch(inputActions.passwordFormIsValid(enteredPassword));
+
+        dispatch(loginActions.saveLogin({
+            email: enteredEmail,
+            password: enteredPassword
+        }));
+
+        dispatch(inputActions.loginInputFormIsValid());
+        
+        if(!login.formIsValid) {
+            setIsSubmitted(false);
+            return;
+        }
+    };
+
+    useEffect(() => {
+        if(login.formIsValid && exist.email && exist.password) {
+            dispatch(inputActions.touchedEmail(false));
+            dispatch(inputActions.touchedPassword(false));
+
+            dispatch(sendUserData({
+                email: beStoredLogin.email,
+                password: beStoredLogin.password
+            }));
+            
+            setEnteredEmail('');
+            setEnteredPassword('');
+            
+            dispatch(uiActions.toggleUserForm(false));
+            dispatch(uiActions.toggleLoggingIn(true));
+        };
+    }, [beStoredLogin]);
+    
     const emailClasses = `${classes['input-box']} ${login.emailIsValid && !touched.email ? '' : classes.invalid}`;
     const passwordClasses = `${classes['input-box']} ${login.passwordIsValid && !touched.password ? '' : classes.invalid}`;
   
@@ -152,19 +137,16 @@ const Login = props => {
         <form className={classes['login-form']} onSubmit={submitHandler}>
             <h1>Login</h1>
             <div className={classes['input-box']}>
-                <label htmlFor='name'>Name</label>
-                <input type='text' id="name" autoComplete='off' value={enteredName} onChange={nameChangeHandler} onBlur={nameBlurHander} />
-                {!login.nameIsValid || touched.name ? <p>Please enter a valid name!</p> : null}
-            </div>
-            <div className={classes['input-box']}>
                 <label htmlFor='email'>E-mail</label>
                 <input type='text' id="email" autoComplete='off' value={enteredEmail} onChange={emailChangeHandler} onBlur={emailBlurHandler} />
                 {!login.emailIsValid || touched.email ? <p>Please enter a valid email!</p> : null}
+                {login.emailIsValid && !exist.email && <p>That account doesn't exist. Enter a different account or get a new one.</p>}
             </div>
             <div className={classes['input-box']}>
                 <label htmlFor='password'>Password</label>
                 <input type='text' id="password" autoComplete='off' value={enteredPassword} onChange={passwordChangeHandler} onBlur={passwordBlurHandler} />
                 {!login.passwordIsValid || touched.password ? <p>Password must be 8-16 characters and contain both numbers and letters/special characters.</p> : null}
+                {login.passwordIsValid && !exist.password && <p>Password incorrect. Forgot password?</p>}
             </div>
             <div className={classes['form-trans']}>
                 <p>Don't have an account?</p>
