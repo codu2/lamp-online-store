02439dbcd9d0a56c087612e94850389473eb54bb
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-03 21:35:24 +0900

    Update OrderForm.js

--- src/components/cart/OrderForm.js
+++ src/components/cart/OrderForm.js
@@ -6,7 +6,8 @@ import { FaLongArrowAltLeft } from 'react-icons/fa';
 import { uiActions } from '../../store/ui-slice';
 import { orderActions } from "../../store/order-slice";
 import { inputActions } from '../../store/input-slice';
-import { sendOrderData } from '../../store/cart-action';
+import { cartActions } from '../../store/cart-slice';
+import { sendOrderData, sendCartData } from '../../store/cart-action';
 
 const OrderForm = () => {
     const dispatch = useDispatch();
@@ -18,14 +19,131 @@ const OrderForm = () => {
     const [enteredStreet, setEnteredStreet] = useState('');
     const [enteredPostalCode, setEnteredPostalCode] = useState('');
     const [enteredCity, setEnteredCity] = useState('');
+    const [isSubmitted, setIsSubmitted] = useState(true);
 
+    const movePrevPageHandler = () => {
+        dispatch(uiActions.closeOrderFrom());
+    };
+    
+    const isNotEmpty = event => event.target.value.trim() !== '';
+    const isFiveNum = /^\d{5}$/;
+    const phoneNumberCheck = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+
+    const phoneNumberChangedHandler = event => {
+        dispatch(inputActions.phoneNumberFormIsValid(event.target.value));
+        if(touched.phoneNumber) {
+            if(phoneNumberCheck.test(event.target.value.trim())){
+                dispatch(inputActions.touchedPhoneNumber(false));
+                dispatch(inputActions.phoneNumberFormIsValid(event.target.value));
+            };
+        };
+        if(!isSubmitted) {
+            if(phoneNumberCheck.test(event.target.value.trim())){
+                dispatch(inputActions.touchedPhoneNumber(false));
+                dispatch(inputActions.phoneNumberFormIsValid(event.target.value));
+            };
+        }
+        setEnteredPhoneNumber(event.target.value);
+    };
+    
+    const streetChangedHandler = event => {
+         dispatch(inputActions.touchedStreet(false));
+        if(touched.street) {
+            if(isNotEmpty) {
+                dispatch(inputActions.streetFormIsValid(event.target.value));
+            };
+        };
+        if(!isSubmitted) {
+            if(isNotEmpty) {
+                dispatch(inputActions.streetFormIsValid(event.target.value));
+            };
+        };
+        setEnteredStreet(event.target.value);
+    };
+    
+    const postalCodeChangedHandler = event => {
+        dispatch(inputActions.postalCodeFormIsValid(event.target.value));
+        if(touched.postalCode) {
+            if(isFiveNum.test(event.target.value)) {
+                dispatch(inputActions.touchedPostalCode(false));
+                dispatch(inputActions.postalCodeFormIsValid(event.target.value));
+            };
+        };
+        if(!isSubmitted) {
+            if(isFiveNum.test(event.target.value)) {
+                dispatch(inputActions.touchedPostalCode(false));
+                dispatch(inputActions.postalCodeFormIsValid(event.target.value));
+            };
+        }
+        setEnteredPostalCode(event.target.value);
+    };
+    
+    const cityChangedHandler = event => {
+        dispatch(inputActions.touchedCity(false));
+        if(touched.city) {
+            if(isNotEmpty) {
+                dispatch(inputActions.cityFormIsValid(event.target.value));
+            };
+        };
+        if(!isSubmitted) {
+            if(isNotEmpty) {
+                dispatch(inputActions.cityFormIsValid(event.target.value));
+            };
+        };
+        setEnteredCity(event.target.value);
+    }; 
+
+    const phoneNumberBlurHandler = event => {
+        if(!phoneNumberCheck.test(event.target.value.trim())){
+            dispatch(inputActions.touchedPhoneNumber(true));
+        } else {
+            dispatch(inputActions.touchedPhoneNumber(false));
+        };
+        dispatch(inputActions.phoneNumberFormIsValid(event.target.value));
+    };
+    
+    const streetBlurHandler = event => {
+        if(event.target.value.trim() === '') {
+            dispatch(inputActions.touchedStreet(true));
+        } else {
+            dispatch(inputActions.touchedStreet(false));
+        };
+        dispatch(inputActions.streetFormIsValid(event.target.value));
+    };
+    
+    const postalCodeBlurHandler = event => {
+         if(!isFiveNum.test(event.target.value)) {
+            dispatch(inputActions.touchedPostalCode(true));
+        } else {
+            dispatch(inputActions.touchedPostalCode(false));
+        };
+        dispatch(inputActions.postalCodeFormIsValid(event.target.value));
+    };
+    
+    const cityBlurHandler = event => {
+        if(event.target.value.trim() === '') {
+            dispatch(inputActions.touchedCity(true));
+        } else {
+            dispatch(inputActions.touchedCity(false));
+        };
+        dispatch(inputActions.cityFormIsValid(event.target.value));
+    }; 
+    
     const submitHandler = event => {
         event.preventDefault();
         
-        dispatch(inputActions.touchedPhoneNumber(true));
-        dispatch(inputActions.touchedStreet(true));
-        dispatch(inputActions.touchedPostalCode(true));
-        dispatch(inputActions.touchedCity(true));
+        if(!order.phoneNumberIsValid) {
+            dispatch(inputActions.touchedPhoneNumber(true));
+        };
+        if(!order.streetIsValid) {
+            dispatch(inputActions.touchedStreet(true));
+        };
+        if(!order.postalCodeIsValid) {
+            dispatch(inputActions.touchedPostalCode(true));
+        };
+        if(!order.cityIsValid) {
+            dispatch(inputActions.touchedCity(true));
+        };
         
         dispatch(inputActions.phoneNumberFormIsValid(enteredPhoneNumber));
         dispatch(inputActions.streetFormIsValid(enteredStreet));
@@ -40,6 +158,11 @@ const OrderForm = () => {
         }));
 
         dispatch(inputActions.orderInputFormIsValid());
+        
+        if(!order.formIsValid) {
+            setIsSubmitted(false);
+            return;
+        };
     };
     
     useEffect(() => {
@@ -60,66 +183,18 @@ const OrderForm = () => {
             setEnteredStreet('');
             setEnteredPostalCode('');
             setEnteredCity('');
-        };
-    }, [beStoredOrder]);
+            
+            dispatch(cartActions.replaceCart({
+                items: [],
+                totalQuantity: 0
+            }));
 
-    const movePrevPageHandler = () => {
-        dispatch(uiActions.closeOrderFrom());
-    };
-    
-    const isNotEmpty = event => event.target.value.trim() !== '';
-    const isFiveNum = /^\d{5}$/;
-    const phoneNumberCheck = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
-
-    const phoneNumberChangedHandler = event => {
-        if(phoneNumberCheck.test(event.target.value.trim()) || isNotEmpty){
-            dispatch(inputActions.touchedPhoneNumber(false));
+            dispatch(sendCartData({
+                items: [],
+                totalQuantity: 0
+            }));
         };
-        setEnteredPhoneNumber(event.target.value);
-        dispatch(inputActions.phoneNumberFormIsValid(enteredPhoneNumber));
-    };
-    const streetChangedHandler = event => {
-        if(isNotEmpty) {
-            dispatch(inputActions.touchedStreet(false));
-        };
-        setEnteredStreet(event.target.value);
-        dispatch(inputActions.streetFormIsValid(enteredStreet));
-    };
-    const postalCodeChangedHandler = event => {
-        if(isFiveNum.test(event.target.value.trim()) || isNotEmpty) {
-            dispatch(inputActions.touchedPostalCode(false));
-        };
-        setEnteredPostalCode(event.target.value);
-        dispatch(inputActions.postalCodeFormIsValid(enteredPostalCode));
-    };
-    const cityChangedHandler = event => {
-        if(isNotEmpty) {
-            dispatch(inputActions.touchedCity(false));
-        };
-        setEnteredCity(event.target.value);
-        dispatch(inputActions.cityFormIsValid(enteredCity));
-    }; 
-
-    const phoneNumberBlurHandler = event => {
-        if(!phoneNumberCheck.test(event.target.value.trim())){
-            dispatch(inputActions.touchedPhoneNumber(true));
-        }
-    };
-    const streetBlurHandler = event => {
-        if(event.target.value.trim() === '') {
-            dispatch(inputActions.touchedStreet(true));
-        }
-    };
-    const postalCodeBlurHandler = event => {
-        if(!isFiveNum.test(event.target.value.trim())) {
-            dispatch(inputActions.touchedPostalCode(true));
-        }
-    };
-    const cityBlurHandler = event => {
-        if(event.target.value.trim() === '') {
-            dispatch(inputActions.touchedCity(true));
-        }
-    }; 
+    }, [beStoredOrder, order.formIsValid, dispatch]);
     
     const phoneNumberClasses = `${classes['input-box']} ${order.phoneNumberIsValid && !touched.phoneNumber ? '' : classes.invalid}`;
     const streetClasses = `${classes['input-box']} ${order.streetIsValid && !touched.street ? '' : classes.invalid}`;
