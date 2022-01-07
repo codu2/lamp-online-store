0e9e4a57b1a32ca1789c9ebfe86f2c32606cbc59
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-08 03:43:09 +0900

    Add files via upload

---
+++ src/pages/ProductPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ProductDetail from '../components/products/ProductDetail';
+
+const ProductPage = () => {
+    return (
+        <section>
+            <ProductDetail />
+        </section>
+    );
+};
+
+export default ProductPage;
