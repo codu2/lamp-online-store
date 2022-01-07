1f693816e1cd63bbf6fa50aaa199d7e3baa9ff4c
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-07 23:12:31 +0900

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Main from '../components/main/Main';
+
+const Home = () => {
+    return (
+        <section>
+            <Main />
+        </section>
+    );
+};
+
+export default Home;
---
+++ src/pages/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ProductList from '../components/products/ProductList';
+
+const Products = () => {
+    return (
+        <section>
+            <ProductList />
+        </section>
+    )
+};
+
+export default Products;
