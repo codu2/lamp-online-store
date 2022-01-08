f0149b24828003917e36331227062b1c8a6066cb
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-09 06:06:20 +0900

    Update ProductItem.js

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import classes from './ProductItem.module.css';
 import { cartActions } from '../../store/cart-slice';
+import { uiActions } from '../../store/ui-slice';
 
 const ProductItem = props => {
    const dispatch = useDispatch();
+   const products = useSelector(state => state.ui.products);
 
-    const { id, name, price, color, img } = props;
+   const { id, name, price, color, img } = props;
 
-    const addItemToCartHandler = () => {
+   const addItemToCartHandler = () => {
         dispatch(cartActions.addItemToCart({
             id,
             name,
@@ -19,11 +22,27 @@ const ProductItem = props => {
         }))
     };
    
+   const productClickHandler = () => {
+        for(const key in products) {
+            if(key === id) {
+                dispatch(uiActions.replaceProductDetail({
+                    id: products[key].id,
+                    name: products[key].name,
+                    price: products[key].price,
+                    color: products[key].color,
+                    img: products[key].img
+                }));
+            };
+        };
+    };
+   
    return (
     <div className={classes['product-item']}>
         <img src={img} alt="lamp" />
         <div className={classes['product-box']}>
-            <div className={classes.name}>{name}</div>
+            <div className={classes.name} onClick={productClickHandler}>
+                <Link to={`/products/${name.replace(/(\s*)/g, "")}`}>{name}</Link>
+            </div>
             <div className={classes['product-info']}>
                 <span>{color}</span>
                 <span>{`$${price.toFixed(2)}`}</span>
