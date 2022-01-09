ca384da80c31dfd4ef7396d157dcd907395f9b49
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-10 01:01:27 +0900

    Add files via upload

---
+++ src/components/header/Layout.js
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+
+import Header from './Header';
+
+const Layout = () => {
+    return (
+        <div>
+            <header>
+                <Header />
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
+};
+
+export default Layout;
