2c7dfa6c6b7a5b5cc8d8ae514e2dd747026bc4cb
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-12 04:58:50 +0900

    Update Products.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 import ProductList from '../components/products/ProductList';
 
 const Products = () => {
     return (
         <section>
+            <Outlet />
             <ProductList />
         </section>
     )
