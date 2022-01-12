ecebe5f53097e974ff205392054c8c6bb23fe6e0
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-13 01:15:47 +0900

    Update ProductList.js

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import classes from './ProductList.module.css';
 import ProductItem from './ProductItem';
@@ -20,14 +21,14 @@ const DUMMY_PRODUCTS = [
     },
     {
         id: 'p3',
-        name: 'Anywhere Lamp',
+        name: 'Anywhere Lamp1',
         price: 40.29,
         img: 'img/lamp3.jpg',
         color: 'Orange&Silver'
     },
     {
         id: 'p4',
-        name: 'Anywhere Lamp',
+        name: 'Anywhere Lamp2',
         price: 40.29,
         img: 'img/lamp4.jpg',
         color: 'white&Black'
@@ -41,19 +42,57 @@ const DUMMY_PRODUCTS = [
     },
 ]
 
-const productsList = (
-    <ul className={classes['products-list']}>
-        {DUMMY_PRODUCTS.map(product => (
-            <ProductItem key={product.id} id={product.id} name={product.name} price={product.price} color={product.color} img={product.img} />
-        ))}
-    </ul>
-)
-
 const ProductList = () => {
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const queryParams = searchParams.get('sort');
+
+    const isSortingWall = queryParams === 'wall';
+
+    const sortProductsList = (products) => {
+        const wallLamps = products.filter(product => product.sort === 'wall');
+        const tableLamps = products.filter(product => product.sort === 'table');
+        return isSortingWall ? wallLamps : tableLamps;
+    };
+
+    const sortedProducts = sortProductsList(DUMMY_PRODUCTS);
+    
+    const changeAllHandler = () => {
+        navigate('/products');
+    };
+
+    const changewallHandler = () => {
+        navigate('/products?sort=wall');
+    };
+
+    const changetableHandler = () => {
+        navigate('/products?sort=table');
+    };
+    
+    const sortList = (
+        sortedProducts.map(product => (
+            <ProductItem key={product.id} id={product.id} name={product.name} price={product.price} color={product.color} img={product.img} />
+        ))
+    );
+
+    const allList = (
+        DUMMY_PRODUCTS.map(product => (
+            <ProductItem key={product.id} id={product.id} name={product.name} price={product.price} color={product.color} img={product.img} />
+        ))
+    );
+
     return (
         <div className={classes['products']}>
             <h1>Products</h1>
-            {productsList}
+            <div className={classes.sorting}>
+                <span>Sort</span>
+                <button onClick={changeAllHandler}>All</button>
+                <button onClick={changewallHandler}>Wall Lamps</button>
+                <button onClick={changetableHandler}>Table Lamps</button>
+            </div>
+            <ul className={classes['products-list']}>
+                {queryParams ? sortList : allList}
+            </ul>
         </div>
     )
 };
