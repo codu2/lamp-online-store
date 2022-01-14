610ebcee8f32c547659302ba374e450f13e5f6ed
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-15 02:28:39 +0900

    Update cart-slice.js

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -4,9 +4,34 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState: {
         items: [],
-        totalQuantity: 0
+        totalQuantity: 0,
+        changed: false
     },
     reducers: {
+        addProductDetailItemToCart(state, action) {
+            const detailItem = action.payload;
+            const existingItem = state.items.find(item => item.id === detailItem.id);
+            if(!existingItem) {
+                state.items.push({
+                    id: detailItem.id,
+                    name: detailItem.name,
+                    price: detailItem.price,
+                    img: detailItem.img,
+                    color: detailItem.color,
+                    quantity: detailItem.quantity,
+                    totalPrice: detailItem.price * detailItem.quantity
+                });
+                state.totalQuantity = state.totalQuantity + detailItem.quantity;
+            } else {
+                state.totalQuantity = state.totalQuantity + detailItem.quantity;
+                existingItem.totalQuantity = existingItem.totalQuantity + detailItem.quantity;
+                existingItem.totalPrice = existingItem.totalPrice + detailItem.price * detailItem.quantity;
+            };
+        },
+        replaceCart(state, action) {
+            state.items = action.payload.items;
+            state.totalQuantity = action.payload.totalQuantity;
+        },
         addItemToCart(state, action) {
             const newItem = action.payload; //object
             const existingItem = state.items.find(item => item.id === newItem.id); 
@@ -22,12 +47,12 @@ const cartSlice = createSlice({
                     quantity: 1, // cart items에 처음 추가하는 것이므로 수량은 1로 정함
                     totalPrice: newItem.price 
                     // 일단 수량이 하나이므로 price 값으로 함. 이 items는 cart 안에 있는 것이므로 totalPrice가 필요함
-                });
+               });
             } else {
-                existingItem.quantity++; // 이미 존재하는 item의 수량은 1만큼 증가시킴
-                existingItem.totalPrice = existingItem.price + newItem.price; 
-                // 해당 item의 totalPrice 값에 1개 만큼의 price 값을 더해줌
-            }
+               existingItem.quantity++; 
+               existingItem.totalPrice = existingItem.totalPrice + newItem.price; 
+            };
+            state.changed = true;
         },
         removeItemFromCart(state, action) {
             const newItemId = action.payload; //이번에는 cart에서 삭제하려는 item의 id를 받음
@@ -38,7 +63,8 @@ const cartSlice = createSlice({
             } else {
                 existingItem.quantity--;
                 existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
-            }
+            };
+            state.changed = true;
         }
     }
 });
