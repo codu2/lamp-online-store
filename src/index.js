be7819654bb92cf02f590fbae82ae8e182449309
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-18 23:37:36 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { AuthContextProvider } from './store/auth-context';
 import store from './store/index';
 
-ReactDOM.render(<BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<AuthContextProvider><BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter></AuthContextProvider>, document.getElementById('root'));
