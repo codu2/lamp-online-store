98eebb5612b81033569442d3e5cd62e6530574a8
codu2 <82283517+codu2@users.noreply.github.com>
2022-01-18 23:39:30 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
@@ -9,12 +9,16 @@ import Products from './pages/Products';
 import ProductPage from './pages/ProductPage';
 import NotFound from './pages/NotFound';
 import { fetchCartData, sendCartData, fetchUsersData, fetchLoginData, fetchProductsData } from './store/cart-action';
+import UserArea from './components/user/UserArea';
+import Profile from './components/user/Profile';
+import AuthContext from './store/auth-context';
 
 let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
   const cart = useSelector(state => state.cart);
+  const authCtx = useContext(AuthContext);
   
   useEffect(() => {
     dispatch(fetchProductsData());
@@ -43,6 +47,8 @@ function App() {
             <Route path="/products" element={<Products />}>
               <Route path=":productId" element={<ProductPage />} />
             </Route>
+            {!authCtx.isLoggedIn && <Route path="/user" element={<UserArea />} />}
+            {authCtx.isLoggedIn && <Route path="/profile" element={<Profile />} />}
           </Route>
           <Route path="*" element={<NotFound />} />
       </Routes>
